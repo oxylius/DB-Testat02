23f8adb40134d3f79f8bce6393b91f27b6f71e9f
lukasst <lukas.steiger@hotmail.com>
2014-10-28 09:22:25 +0100

    Daten für alle Tabellen (ausg. Artikel) hinzugefügt

--- Scripts/3_inserts.sql
+++ Scripts/3_inserts.sql
@@ -1,91 +1,77 @@
+INSERT INTO Gestell VALUES
+	(1, 'A1', 14),
+	(2, 'A2', 45),
+	(3, 'A3', 12),
+	(4, 'B1', 1),
+	(5, 'B2', 22),
+	(6, 'B3', 33),
+	(7, 'C1', 1),
+	(8, 'C2', 31)	
+;
 
-/*
- * Testdaten einfüllen
- */
--- TABLE  ABTEILUNG
+INSERT INTO Produkt VALUES
+	(1, 'Apfel', 'Eine süsse oder saure Frucht von IHREN Schweizer Bauern'),
+	(2, 'Birne', 'Eine süsse oder saure Frucht von IHREN Schweizer Bauern'),
+	(3, 'Erdbeere', 'Die süsse Verführung'),
+	(4, 'Ananas', 'Exotische Frucht, produziert und geernet von Armen Menschen'),
+	(5, 'Banane', 'Exotische Frucht, produziert und geernet von Armen Menschen'),
+	(6, 'Mango', 'Exotische Frucht, produziert und geernet von Armen Menschen'),
+	(7, 'Kirsche', 'Eine süsse oder saure Frucht von IHREN Schweizer Bauern')
+;
 
-INSERT INTO abteilung VALUES (1, 'Verkauf');
-INSERT INTO abteilung VALUES (2, 'Marketing');
-INSERT INTO abteilung VALUES (3, 'Entwicklung');
-INSERT INTO abteilung VALUES (4, 'Finanzen');
-INSERT INTO abteilung VALUES (5, 'QS');
- 
--- TABLE  ANGESTELLTER
+INSERT INTO Person VALUES
+	(1, 'Hansi', 'Hinterseher', '1.1.1900'),
+	(2, 'Helene', 'Fischer', '22.3.1991'),
+	(3, 'Vujo', 'Gavric', '12.1.2005'),
+	(4, 'Mike', 'Shiva', '13.12.1950'),
+	(5, 'Monika', 'Fasnacht', '1.2.1939'),
+	(6, 'DJ', 'Bobo', '12.8.1977'),
+	(7, 'Melanie', 'Winniger', '18.9.1966'),
+	(8, 'Jacqueline', 'Badran', '1.5.1972'),
+	(9, 'Cedric', 'Wehrmuth', '12.12.1999'),
+	(10, 'Barack', 'Obama', '18.5.1977')
+;
 
-INSERT INTO angestellter (persnr, name, tel, salaer, chef, abtnr, wohnort) VALUES
-(1001, 'Marxer, Markus', 234, 10580.00, NULL, 1, 'Luzern');
-INSERT INTO angestellter (persnr, name, tel, salaer, chef, abtnr, wohnort) VALUES
-(1002, 'Widmer, Anna', 301, 12010.00, NULL, 2, 'Adligenswil');
-INSERT INTO angestellter (persnr, name, tel, salaer, chef, abtnr, wohnort) VALUES
-(1010, 'Steiner, Hans', 409, 10867.00, NULL, 3, 'Hitzkirch');
-INSERT INTO angestellter (persnr, name, tel, salaer, chef, abtnr, wohnort) VALUES
-(1019, 'Affolter, Vreni', 233, 4123.00, 1001, 1, 'Zürich');
-INSERT INTO angestellter (persnr, name, tel, salaer, chef, abtnr, wohnort) VALUES
-(1100, 'Widmer, Karl', 450, 7500.00, 1010, 3, 'Emmen');
-INSERT INTO angestellter (persnr, name, tel, salaer, chef, abtnr, wohnort) VALUES
-(1123, 'Meier, Franz', 240, 9765.00, 1001, 1, 'Zug');
-INSERT INTO angestellter (persnr, name, tel, salaer, chef, abtnr, wohnort) VALUES
-(2098, 'Zürcher, Hedi', 249, 10019.00, 1001, 1, 'Horw');
-INSERT INTO angestellter (persnr, name, tel, salaer, chef, abtnr, wohnort) VALUES
-(2109, 'Heiniger, Urs', 345, 4098.00, 1002, 2, 'Luzern');
-INSERT INTO angestellter (persnr, name, tel, salaer, chef, abtnr, wohnort) VALUES
-(2298, 'Pauli, Monika', 478, 5089.00, 1010, 3, 'Luzern');
-INSERT INTO angestellter (persnr, name, tel, salaer, chef, abtnr, wohnort) VALUES
-(2345, 'Becker, Fritz', 310, 6346.00, 1002, 2, 'Hochdorf');
-INSERT INTO angestellter (persnr, name, tel, salaer, chef, abtnr, wohnort) VALUES
-(2567, 'Ammann, Fritz', 467, 7890.00, 1010, 3, 'Baar');
-INSERT INTO angestellter (persnr, name, tel, salaer, chef, abtnr, wohnort) VALUES
-(2601, 'Wehrli, Anton', NULL, 5980.00, 2567, 3, 'Luzern');
-INSERT INTO angestellter (persnr, name, tel, salaer, chef, abtnr, wohnort) VALUES
-(2666, 'Beeler, Hans', NULL, 3780.00, 2567, 3, 'Rotkreuz');
-INSERT INTO angestellter (persnr, name, tel, salaer, chef, abtnr, wohnort) VALUES
-(2701, 'Graber, Berta', 451, 4590.00, 1100, 3, 'Sursee');
-INSERT INTO angestellter (persnr, name, tel, salaer, chef, abtnr, wohnort) VALUES
-(3000, 'Aarburg, Werner', 400, 9000.00, 1100, 3, 'Abtwil');
-INSERT INTO angestellter (persnr, name, tel, salaer, chef, abtnr, wohnort) VALUES
-(3019, 'Kern, Veronika', 401, 4800.00, 3000, 3, 'Sins');
-INSERT INTO angestellter (persnr, name, tel, salaer, chef, abtnr, wohnort) VALUES
-(3333, 'Wernli, Peter', 112, 8978.00, 1001, 1, 'Luzern');
-INSERT INTO angestellter (persnr, name, tel, salaer, chef, abtnr, wohnort) VALUES
-(3338, 'Kramer, Luise', NULL, 4000.00, 3000, 3, 'Luzern');
-INSERT INTO angestellter (persnr, name, tel, salaer, chef, abtnr, wohnort) VALUES
-(4000, 'Rey, Herbert', 480, 15000.00, NULL, 4, 'Adligenswil');
-INSERT INTO angestellter (persnr, name, tel, salaer, chef, abtnr, wohnort) VALUES
-(4010, 'Danuser, Vreni', 481, 5100.00, 4000, 4, 'Luzern');
-INSERT INTO angestellter (persnr, name, tel, salaer, chef, abtnr, wohnort) VALUES
-(2000, 'Schnell, Marie', 601, 5100.00, 4000, 4, 'Luzern');
-INSERT INTO angestellter (persnr, name, tel, salaer, chef, abtnr, wohnort) VALUES
-(2010, 'Gschwind, Fritz', 602, 5900.00, 4000, 4, 'Luzern');
-INSERT INTO angestellter (persnr, name, tel, salaer, chef, abtnr, wohnort) VALUES
-(2020, 'Test, Hans', 602, 5900.00, 4000, 4, 'Luzern');
- 
--- TABLE  ABTLEITUNG (ABTCHEF, ABTNR)
+INSERT INTO Mitarbeiter VALUES
+	(1,1),
+	(2,2),
+	(3,3),
+	(4,4),
+	(5,5),
+	(6,6),
+	(7,7)
+;
 
-INSERT INTO abtleitung VALUES (1, 1001);
-INSERT INTO abtleitung VALUES (2, 1002);
-INSERT INTO abtleitung VALUES (3, 1010);
-INSERT INTO abtleitung VALUES (4, 3000);
- 
--- TABLE  PROJEKT 
+INSERT INTO Kunde VALUES
+	(1,10),
+	(2,9),
+	(3,8),
+	(4,4),
+	(5,5),
+	(6,6),
+	(7,7)
+;
 
-INSERT INTO projekt VALUES (25, 'Saturn              ', to_date('1994-01-01', 'YYYY-MM-DD'), 30, 120, 1001);
-INSERT INTO projekt VALUES (26, 'Mars                ', to_date('1992-12-01', 'YYYY-MM-DD'), 500, 1000, 1100);
-INSERT INTO projekt VALUES (27, 'Uranus              ', NULL, NULL, NULL, 1100);
-INSERT INTO projekt VALUES (30, 'Jupiter             ', to_date('1993-02-12', 'YYYY-MM-DD'), 10, 50, 1001);
- 
--- TABLE  ProjektZuteilung (PERSNR,PROJNR,ZEITANTEIL, STARTZEIT, DAUER)
+INSERT INTO Bestellung VALUES
+	(1, '25.10.2014', 1),
+	(2, '26.10.2014', 2),
+	(3, '27.10.2014', 3),
+	(4, '28.10.2014', 4),
+	(5, '29.10.2014', 5),
+	(6, '29.10.2014', 6),
+	(7, '29.10.2014', 7)
+;
 
-INSERT INTO projektzuteilung VALUES (1001, 26, 30, NULL, NULL);
-INSERT INTO projektzuteilung VALUES (1001, 27, 30, NULL, NULL);
-INSERT INTO projektzuteilung VALUES (1001, 30, 10, NULL, NULL);
-INSERT INTO projektzuteilung VALUES (1019, 25, 90, NULL, NULL);
-INSERT INTO projektzuteilung VALUES (1100, 26, 50, NULL, NULL);
-INSERT INTO projektzuteilung VALUES (1123, 25, 50, to_date('1994-01-01', 'YYYY-MM-DD'), NULL);
-INSERT INTO projektzuteilung VALUES (1123, 30, 90, NULL, NULL);
-INSERT INTO projektzuteilung VALUES (2098, 26, 20, NULL, NULL);
-INSERT INTO projektzuteilung VALUES (2098, 27, 31, NULL, NULL);
-INSERT INTO projektzuteilung VALUES (2298, 26, 89, NULL, NULL);
-INSERT INTO projektzuteilung VALUES (2601, 25, 30, NULL, NULL);
-INSERT INTO projektzuteilung VALUES (2601, 26, 40, NULL, NULL);
-INSERT INTO projektzuteilung VALUES (2601, 30, 40, NULL, NULL);
-INSERT INTO projektzuteilung VALUES (2701, 26, 90, NULL, NULL);
+INSERT INTO ProduktBestellung VALUES
+	(1, 3, 1, 1),
+	(2, 14, 1, 4),
+	(3, 2, 2, 3),
+	(4, 1, 3, 5),
+	(5, 17, 4, 2),
+	(6, 1777, 4, 6),
+	(7, 22, 5, 1),
+	(8, 3, 6, 3),
+	(9, 8, 7, 6),
+	(10, 13, 7, 7)
+;
+
